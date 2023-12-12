6aea55a511ddef34bc370b5791cbdad54d042b2c
its-samarth <samarthkasma21@gmail.com>
2023-12-12 19:25:02 +0530

    mvp local auth

---
+++ App.js
@@ -0,0 +1,32 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import * as LocalAuthentication from 'expo-local-authentication';
+
+export default function App() {
+  let [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    async function authenticate() {
+      const result = await LocalAuthentication.authenticateAsync();
+      setIsAuthenticated(result.success);
+    }
+    authenticate();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Text>{isAuthenticated ? "Here's some sensitive info!" : "Uh oh! Access Denied"}</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
